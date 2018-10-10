a50bfb3b819f517b850f036b26076e54cf1dc9e7
Millmoss <omelliff@yahoo.com>
2018-10-09 22:45:49 -0400

    He can make it through obstacle course, need to fix minor things

--- Assets/Scripts/UnitPath.cs
+++ Assets/Scripts/UnitPath.cs
@@ -12,13 +12,20 @@ public class UnitPath : MonoBehaviour
 	public float moveSpeed = 1;
 	private Vector3 movement;
 	private float t = 0;
-	public GameObject radiusDisplay;
-	public GameObject seeLine;
-	public GameObject blindLine;
+	public GameObject radius;
+	public GameObject lineA;
+	public GameObject lineB;
+	public GameObject lineC;
+	public GameObject lineD;
+	public Material greenMat;
+	public Material redMat;
 	public string unitName;
 	public UIHandler uiReceive;
 	public Vector3 velocity;
 	public float acceleration;
+	public float rayDistance = 1f;
+	public float goalRotatePower = .05f;
+	public float avoidRotatePower = .5f;
 
 	void Start()
 	{
@@ -43,26 +50,116 @@ public class UnitPath : MonoBehaviour
 
 	void FixedUpdate()
 	{
+		//rotation
+
+		steer();
+		head.transform.rotation = transform.rotation;
+
 		//movement
 
-		velocity += (((GameObject)pathGoals[pathIndex]).transform.position - transform.position) * acceleration * Time.deltaTime;
+		velocity += transform.forward * acceleration * Time.deltaTime;
 		velocity = Vector3.ClampMagnitude(velocity, moveSpeed);
 		transform.position += velocity * Time.deltaTime;
 
 		//y axis
 
 		transform.position = new Vector3(transform.position.x, 0 + .35f, transform.position.z);
+
+		//pathing logic
+
+		if (pathGoals.Count > pathIndex && Vector3.Distance(((GameObject)pathGoals[pathIndex]).transform.position, transform.position) < rayDistance)
+			pathIndex++;
 	}
 
-	void displayInfo()
+	void steer()
 	{
+		if (pathGoals.Count <= pathIndex)
+			return;
 
+		Quaternion goalRotation = Quaternion.LookRotation(((GameObject)pathGoals[pathIndex]).transform.position - transform.position);
+		float rr = rayResult();
+		Quaternion avoidRotation = transform.rotation * Quaternion.AngleAxis(rr * 5, Vector3.up);
+
+		transform.rotation = Quaternion.Slerp(transform.rotation, goalRotation, goalRotatePower * Time.deltaTime);
+		transform.rotation = Quaternion.Slerp(transform.rotation, avoidRotation, avoidRotatePower * Time.deltaTime);
 	}
 
-	void radiusSet(float rad, Vector3 pos)
+	float rayResult()	//returns negative for turn power left, positive for turn power right, zero for no change of course
 	{
-		radiusDisplay.transform.position = pos;
-		radiusDisplay.transform.localScale = new Vector3(rad, .05f, rad);
+		LayerMask selfMask = 1 << 0;
+
+		Ray rayA = new Ray(transform.position - transform.right * .25f, transform.forward);
+		RaycastHit hitA = new RaycastHit();
+		bool a = Physics.Raycast(rayA, out hitA, rayDistance, selfMask);
+		rayDisplay(lineA, rayA.origin, rayA.origin + rayA.direction * rayDistance, a);
+
+		Ray rayB = new Ray(transform.position + transform.right * .25f, transform.forward);
+		RaycastHit hitB = new RaycastHit();
+		bool b = Physics.Raycast(rayB, out hitB, rayDistance, selfMask);
+		rayDisplay(lineB, rayB.origin, rayB.origin + rayB.direction * rayDistance, b);
+
+		Ray rayC = new Ray(transform.position, (transform.forward - transform.right * .15f));
+		RaycastHit hitC = new RaycastHit();
+		bool c = Physics.Raycast(rayC, out hitC, rayDistance, selfMask);
+		rayDisplay(lineC, rayC.origin, rayC.origin + rayC.direction * rayDistance, c);
+
+		Ray rayD = new Ray(transform.position, (transform.forward + transform.right * .15f));
+		RaycastHit hitD = new RaycastHit();
+		bool d = Physics.Raycast(rayD, out hitD, rayDistance, selfMask);
+		rayDisplay(lineD, rayD.origin, rayD.origin + rayD.direction * rayDistance, d);
+
+		//decision logic
+
+		if (!a && !b && !c && !d)
+			return 0;
+
+		float distanceA = Vector3.Distance(hitA.point, rayA.origin);
+		float distanceB = Vector3.Distance(hitB.point, rayB.origin);
+		float distanceC = Vector3.Distance(hitC.point, rayC.origin);
+		float distanceD = Vector3.Distance(hitD.point, rayD.origin);
+
+		if (c && d)
+		{
+			if (distanceC < distanceD)
+				return rayDistance / distanceC;
+			else
+				return rayDistance / -distanceD;
+		}
+		if(c)
+			return rayDistance / distanceC;
+		if (d)
+			return rayDistance / -distanceD;
+
+		if (a && b)
+		{
+			if (distanceA < distanceB)
+				return rayDistance / distanceA;
+			else
+				return rayDistance / -distanceB;
+		}
+		if (a)
+			return rayDistance / distanceA;
+		if (b)
+			return rayDistance / -distanceB;
+
+		return 0;
+	}
+
+	void rayDisplay(GameObject line, Vector3 origin, Vector3 end, bool didHit)
+	{
+		line.transform.position = (origin + end) / 2;
+		line.transform.rotation = Quaternion.LookRotation(end - origin);
+		line.transform.localScale = new Vector3(.05f, .05f, Vector3.Distance(origin, end));
+		if (!didHit)
+			line.GetComponent<MeshRenderer>().material = greenMat;
+		else
+			line.GetComponent<MeshRenderer>().material = redMat;
+	}
+
+	void radiusDisplay(float rad, Vector3 pos)
+	{
+		radius.transform.position = pos;
+		radius.transform.localScale = new Vector3(rad, .05f, rad);
 	}
 
 	void chaseFleeSet()
@@ -112,15 +209,5 @@ public class UnitPath : MonoBehaviour
 			seeLine.transform.position = Vector3.zero;
 		}
 		*/
-	}
-
-	void resetDisplay()
-	{
-		radiusDisplay.transform.position = Vector3.zero;
-		radiusDisplay.transform.localScale = new Vector3(1, .05f, 1);
-		blindLine.transform.position = Vector3.zero;
-		blindLine.transform.localScale = Vector3.zero;
-		seeLine.transform.position = Vector3.zero;
-		seeLine.transform.localScale = Vector3.zero;
 	}
 }
